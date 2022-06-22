e19bd3d9e6de6c9260958677d0de48dc17bfb987
shwartzie <ronnybusiness77@gmail.com>
2022-06-22 20:14:11 +0300

    added css

--- js/app.controller.js
+++ js/app.controller.js
@@ -75,7 +75,7 @@ function buildTable(locations) {
     elTBody.innerHTML = strHTML
 }
 function codeAddress() {
-    mapService.loadAdress()
+    mapService.loadAddress()
 }
 
 function onDelete(id) {
